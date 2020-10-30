a51dc23364bccb9aae6530e51b9162989e6ebd18
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-30 17:19:18 -0200

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -1,21 +1,54 @@
-let meuObjeto = {
-    arr: [ 1, 2, 3 ],
+const img = document.getElementById('img')
+const btn = document.getElementById('btn')
 
-    forEach: function(callback){
-        for(let i = 0; i < callback.length; i++){
-            console.log(this.arr[i])
-            callback(this.arr[i])
-        }
-    }
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
 }
 
-function printaQuadrado( callback ){
-    console.log( callback * callback)
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changeColor = () => {
+    const colors = ['red','yellow','green']
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
 }
 
-function printaMetade( callback ){
-    console.log( callback / 2 )
+const stopAutomatic = () => {
+    clearInterval ( intervalId );
 }
 
-meuObjeto.forEach(printaQuadrado)
-meuObjeto.forEach(printaMetade)
+const turnOn = {
+    'red':      () => img.src = './img/vermelho.png',
+    'yellow':   () => img.src = './img/amarelo.png',
+    'green':    () => img.src = './img/verde.png',
+    'automatic': () => intervalId = setInterval( changeColor, 1000 )
+}
+
+buttons.addEventListener('click', trafficLight );
+
+// let meuObjeto = {
+//     arr: [ 1, 2, 3 ],
+
+//     forEach: function(callback){
+//         for(let i = 0; i < callback.length; i++){
+//             console.log(this.arr[i])
+//             callback(this.arr[i])
+//         }
+//     }
+// }
+
+// function printaQuadrado( callback ){
+//     console.log( callback * callback)
+// }
+
+// function printaMetade( callback ){
+//     console.log( callback / 2 )
+// }
+
+// meuObjeto.forEach(printaQuadrado)
+// meuObjeto.forEach(printaMetade)
